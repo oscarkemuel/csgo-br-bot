0471d0d7af5f1d6c369636aa3a11fcf01face18f
oscarkemuell <oscar.kemuel5@gmail.com>
2021-04-09 03:38:39 -0300

    factoring code

--- index.js
+++ index.js
@@ -7,10 +7,51 @@ const app = express();
 const TinyURL = require('tinyurl');
 
 const axios = require('axios').default
-const data = require('./data.json');
+const teams = require('./data.json');
 const Telegram = require('telegraf').Telegram
 
 app.use(cors())
+
+async function getMatches(){
+  let matches = [];
+  const response = await axios.get('https://hltv-api.vercel.app/api/matches')
+  const data = response.data
+
+  teams.teams.forEach(team => {
+    const games = data.filter((e) => e.teams[0].name == team || e.teams[1].name == team)
+    const noGame = {
+      message: `${team}: sem jogos próximos`,
+      time: new Date
+    }
+    games.length > 0 ? matches.push(games[0]) : matches.push(noGame)
+  });
+
+  async function returnNewLink(url){
+    let newLink;
+    await TinyURL.shorten(`${url}`).then(function(res) { newLink = res })
+    return newLink;
+  }
+
+  async function newLink(){
+    await Promise.all(matches.map(async (e) => {
+      if(e.id){
+        url = `https://www.hltv.org${e.link}`
+        const newLink = await returnNewLink(url)
+        e.link = newLink
+      }
+    }))
+  }
+
+  await newLink()
+
+  matches.sort(function compare(a, b) {
+    var dateA = new Date(a.time);
+    var dateB = new Date(b.time);
+    return dateA - dateB;
+  });
+
+  return matches;
+}
 
 app.get('/send-message', (req, res) => {
   async function createMessage(matches){
@@ -19,24 +60,21 @@ app.get('/send-message', (req, res) => {
                     
     await Promise.all(matches.map(async (matche) => {
       // format date
-      let date = matche.date.toLocaleDateString("pt-BR", {timeZone: "America/Sao_Paulo"});
+      const dateNew = new Date(matche.time)
+      let date = dateNew.toLocaleDateString("pt-BR", {timeZone: "America/Sao_Paulo"});
       if (date == new Date().toLocaleDateString("pt-BR", {timeZone: "America/Sao_Paulo"})){
         date = 'HOJE'
       }
   
-      // format start-time
-      let time = matche.date.toLocaleTimeString("pt-BR", {timeZone: "America/Sao_Paulo"})
+      // // format start-time
+      let time = dateNew.toLocaleTimeString("pt-BR", {timeZone: "America/Sao_Paulo"})
       time = time.substring(0, 5);
-  
-      if(matche.game){
-        const bodyMessage = `\n${matche.name_1} vs ${matche.name_2}\n${date} - ${time}\n${matche.event} \n`;
-        
-        await TinyURL.shorten(`${matche.url}`).then(function(res) {
-          message = message.concat('------------------------------------------------')
-          message = message.concat(bodyMessage)
-          message = message.concat(`Link: ${res}\n`)
-        })
 
+      if(matche.id){
+        message = message.concat('------------------------------------------------\n')
+        const bodyMessage = `${matche.teams[0].name} vs ${matche.teams[1].name}\n${date} - ${time}\n${matche.event.name} \n`;
+        message = message.concat(bodyMessage)
+        message = message.concat(`${matche.link}\n`)
       }else{
         message = message.concat(`${matche.message}\n`)
       }
@@ -46,53 +84,17 @@ app.get('/send-message', (req, res) => {
     return message;
   }
   
-  async function getMatchesBR(team){
-    const response = await axios.get('https://hltv-api.vercel.app/api/matches')
-    const data = response.data
-    const teamName = data.filter((matche) => (matche.teams[0].name == team || matche.teams[1].name == team))
-    const matche = teamName[0];
-
-    if (matche == undefined){
-      const body = {
-        game: false,
-        message: `${team}: sem jogos previstos`,
-        date: new Date()
-      }
-      return body;
-    }
-
-    const body = {
-      game: true,
-      event: matche.event.name,
-      name_1: matche.teams[0].name,
-      name_2: matche.teams[1].name,
-      date: new Date(matche.time),
-      url: 'https:/www.hltv.org' + matche.link,
-    }
-
-    return body;
-  }
-  
   async function getMessage() {
     try {
-      const teams = data.teams;
-      const matches = [];
-    
-      const promises = teams.map(async (team) => {
-        const matcheMessage = await getMatchesBR(team)
-        matches.push(matcheMessage);
-      })
-    
-      await Promise.all(promises);
-    
-      const sortedMatches = matches.slice().sort((a, b) => b.date - a.date);
-      const message = await createMessage(sortedMatches);
+      const matches = await getMatches();
+      const message = await createMessage(matches);
     
       const telegram = new Telegram(process.env.APP_TOKEN)
       telegram.sendMessage(process.env.CHAT_ID, message)
-      // console.log(message)
+      console.log(message)
       res.send('Mensagem enviada!')
     } catch (error) {
+      console.log(error);
       res.send('Houve algum problema!!')
     }
   }
